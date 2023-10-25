a09702a1d8cfee4407bd0ac084583888a916e893
campusricardo <campusricardo2023@gmail.com>
2023-10-25 01:26:50 -0500

    feat: :sparkles: Finish the course

--- public/app.js
+++ public/app.js
@@ -17,7 +17,6 @@ const me = {
 const greetPerson = (person) => {
     console.log(`Hello ${person.name}`);
 };
-greetPerson(me);
 // const invOne = new Invoice('Ricardo', 'Work in BAM', 250);
 // const invTwo = new Invoice('Luigi', 'Works as a plumber', 300);
 // console.log(invOne);
@@ -46,12 +45,84 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [toFrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
+/*
+// GENERICS
+
+
+const addUID = <T extends {name: string}>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return ({...obj, uid});
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+
+//let docTwo = addUID('hello');
+
+console.log(docOne.name);
+
+// with interfaces
+
+interface Resource<T> {
+    uid: number;
+    resourceName: string;
+    data: T;
+}
+
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'Juan'}
+
+}
+
+const docFour: Resource<string[]> = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk', 'toilet roll']
+}
+
+console.log(docThree);
+console.log(docFour);
+*/
+// ENUMS
+// an enum is like an Array 
+var resourceType;
+(function (resourceType) {
+    resourceType[resourceType["BOOK"] = 0] = "BOOK";
+    resourceType[resourceType["AUTHOR"] = 1] = "AUTHOR";
+    resourceType[resourceType["FILM"] = 2] = "FILM";
+    resourceType[resourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    resourceType[resourceType["PERSON"] = 4] = "PERSON";
+})(resourceType || (resourceType = {}));
+;
+const docOne = {
+    uid: 1,
+    resourceType: resourceType.BOOK,
+    data: { name: 'Juan' }
+};
+const docTwo = {
+    uid: 2,
+    resourceType: resourceType.AUTHOR,
+    data: ['bread', 'milk', 'toilet roll']
+};
+// Tuples
+// This is and array
+let arr = ['ryu', 25, true];
+arr[0] = true;
+arr[1] = 'yoshi';
+let tup = ['ryu', 25, true];
+tup[0] = 'Hola';
+// This is a tuple it defines the value of the objects within the array 
+let student;
+student = ['Xia-xia', 12323];
--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,6 @@ const greetPerson = (person: IsPerson) => {
 
 };
 
-greetPerson(me);
 
 // const invOne = new Invoice('Ricardo', 'Work in BAM', 250);
 // const invTwo = new Invoice('Luigi', 'Works as a plumber', 300);
@@ -91,23 +90,94 @@ const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e)=> { 
     e.preventDefault();
+    let values: [string, string, number];
+    values = [toFrom.value, details.value, amount.valueAsNumber];
 
     let doc: HasFormatter;
     if (type.value === 'invoice') {
-        doc = new Invoice(
-        toFrom.value, 
-        details.value,
-        amount.valueAsNumber
-        );
+        doc = new Invoice(...values);
     } else { 
-        doc = new Payment(
-            toFrom.value, 
-            details.value,
-            amount.valueAsNumber
-            );
+        doc = new Payment(...values);
     }
 
     list.render(doc, type.value, 'end');
     
 });
+/*
+// GENERICS
+
+
+const addUID = <T extends {name: string}>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return ({...obj, uid});
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+
+//let docTwo = addUID('hello');
+
+console.log(docOne.name);
+
+// with interfaces
+
+interface Resource<T> {
+    uid: number;
+    resourceName: string;
+    data: T;
+}
+
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'Juan'}
+
+}
+
+const docFour: Resource<string[]> = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk', 'toilet roll']
+}
+
+console.log(docThree);
+console.log(docFour);
+*/
+// ENUMS
+// an enum is like an Array 
+enum resourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON };
+
+interface Resource<T> {
+    uid: number;
+    resourceType: resourceType;
+    data: T;
+}
+
+const docOne: Resource<object> = {
+    uid: 1,
+    resourceType: resourceType.BOOK,
+    data: { name: 'Juan'}
+
+}
+
+const docTwo: Resource<string[]> = {
+    uid: 2,
+    resourceType: resourceType.AUTHOR,
+    data: ['bread', 'milk', 'toilet roll']
+}
+
+
+// Tuples
+
+// This is and array
+let arr:(string|number|boolean)[] = ['ryu', 25, true];
+arr[0] = true;
+arr[1] = 'yoshi';
+
+let tup: [string, number, boolean] = ['ryu', 25, true];
+
+tup[0] = 'Hola';
+// This is a tuple it defines the value of the objects within the array 
+let student: [string, number];
+
+student = ['Xia-xia', 12323];
 
